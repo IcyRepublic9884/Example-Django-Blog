e46830e59d9a260eae1951fc5a6cdf1c5c8c21bf
Icy_Republic9884 <74353534+IcyRepublic9884@users.noreply.github.com>
2021-02-17 10:10:24 +0530

    Minor Changes, edited HTML

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Post:
+class Post(models.Model):
     author = models.CharField(max_length=200)
     title = models.CharField(max_length=80)
     date_posted = models.DateField(auto_now=True)
