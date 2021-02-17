ffa950792e165acf39c38690176b92a131b38add
Icy_Republic9884 <74353534+IcyRepublic9884@users.noreply.github.com>
2021-02-17 13:36:53 +0530

    Template Inheritance and Support to render the Post model onto screen

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import Http404
+from .models import Post
 
 def index(request, *args, **kwargs):
-    return render(request, 'pages/index.html', {'title': 'Random Bullshit'})
+    posts = Post.objects.all()
+    return render(request, 'pages/index.html', {'title': 'Random Bullshit', 'posts': posts})
+
+def about(request, *args, **kwargs):
+    # Render the about page
+    return render(request, 'pages/about.html', {})
+
+def contact(request, *args, **kwargs):
+    # Render the contact pages
+    return render(request, 'pages/contact.html', {})
