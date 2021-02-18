fd4f81103e01e4295700bd243d4144e23e00a601
Icy_Republic9884 <74353534+IcyRepublic9884@users.noreply.github.com>
2021-02-18 16:46:13 +0530

    Forms Styled, Database Optimized

--- pages/forms.py
+++ pages/forms.py
@@ -6,8 +6,14 @@ class CreatePostForm(forms.Form):
     post_content = forms.CharField(widget=forms.Textarea)
     post_author = forms.CharField(label="Post As", max_length=100)
 
+    def __str__(self):
+        return self.post_title
+
 # Trying Something new here
 class PostForm(forms.ModelForm):
+    title = forms.CharField(max_length=80, widget=forms.TextInput(attrs={'class': 'form-control '}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control '}))
+    author = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control '}))
     class Meta:
         model = Post
         fields = ['author', 'title', 'content']
