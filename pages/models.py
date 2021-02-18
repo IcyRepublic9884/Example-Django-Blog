4d773d89e770aa83c3af050be3f808ca40f88a60
Icy_Republic9884 <74353534+IcyRepublic9884@users.noreply.github.com>
2021-02-18 20:31:47 +0530

    Reverted

---
+++ pages/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Post(models.Model):
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=80)
+    date_posted = models.DateField(auto_now=True)
+    content = models.TextField()
