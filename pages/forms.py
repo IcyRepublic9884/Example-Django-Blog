a4bab99dc2fb79dd761340f6586bd6693897e733
Icy_Republic9884 <74353534+IcyRepublic9884@users.noreply.github.com>
2021-02-19 07:23:39 +0530

    Error-Commit

--- pages/forms.py
+++ pages/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import Post
 
+# Trying Something new here
 class PostForm(forms.ModelForm):
-    """Form for creating a post"""
     title = forms.CharField(max_length=80, widget=forms.TextInput(attrs={'class': 'form-control '}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control '}))
     author = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control '}))
