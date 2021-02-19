2f163459e5a4a1e8bf13f69c270ce797781b2172
Icy_Republic9884 <74353534+IcyRepublic9884@users.noreply.github.com>
2021-02-19 09:37:58 +0530

    Small Updates

--- users/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=80, widget=forms.TextInput(attrs={'class': 'form-control '}))
-    password = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control '}))
